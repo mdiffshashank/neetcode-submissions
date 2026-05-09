35b6d070ffbe154cc53d256b0888fdae51dfe3ed
Shashank Trivedi <51978440+mdiffshashank@users.noreply.github.com>
2026-05-09 13:31:10 +0530

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.py
@@ -0,0 +1,14 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        #NGE
+        n = len(temperatures)
+        result = [0] * n
+        stack = []
+
+        for i in range(n):
+            while stack and temperatures[stack[-1]] < temperatures[i]:
+                idx = stack.pop()
+                result[idx] = i - idx
+            stack.append(i)
+        return result
+        
